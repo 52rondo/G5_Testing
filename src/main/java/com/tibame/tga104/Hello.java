f4f047ffb18ed366dfab7a2a7e697e8835df6d6c
52rondo <52rondo@gmail.com>
2022-11-30 14:55:18 +0800

    偷偷修改JOSH DONG + HELLO.java來衝突喽

--- src/main/java/com/tibame/tga104/Hello.java
+++ src/main/java/com/tibame/tga104/Hello.java
@@ -21,5 +21,7 @@ public class Hello extends HttpServlet {
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
+//		aaaaaaaaa
+		//////////
 	}
 }

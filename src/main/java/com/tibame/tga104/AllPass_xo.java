0066d37aa6826dfdc6c4798926a0c9b80cc49041
xo <kkiiki20@gmail.com>
2022-11-30 15:25:05 +0800

    fetch 測試提供?

--- src/main/java/com/tibame/tga104/AllPass_xo.java
+++ src/main/java/com/tibame/tga104/AllPass_xo.java
@@ -3,7 +3,7 @@ package com.tibame.tga104;
 public class AllPass_xo {
 
 	public static void main(String[] args) {
-		System.out.println("時間好趕  祝大家都寫的完XD");
+		System.out.println("時間好趕  祝大家都寫的完XD11");
 		System.out.println("我比較想報前端班XD");
 
 	}

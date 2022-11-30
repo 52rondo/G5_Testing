988060265b2d8ac576721d8f3aa2e4f26033aec6
xo <kkiiki20@gmail.com>
2022-11-30 14:45:35 +0800

    add allpass祝福

---
+++ src/main/java/com/tibame/tga104/AllPass_xo.java
@@ -0,0 +1,11 @@
+package com.tibame.tga104;
+
+public class AllPass_xo {
+
+	public static void main(String[] args) {
+		System.out.println("時間好趕  祝大家都寫的完XD");
+		System.out.println("我比較想報前端班XD");
+
+	}
+
+}

a1082880e141317b1589b5b66a5adc1eac60ed6a
xo <kkiiki20@gmail.com>
2022-11-30 15:15:12 +0800

    相撞練習

--- src/main/java/com/tibame/tga104/Hello.java
+++ src/main/java/com/tibame/tga104/Hello.java
@@ -17,6 +17,7 @@ public class Hello extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("Hello");
+		System.out.println("相撞");
 	}
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
